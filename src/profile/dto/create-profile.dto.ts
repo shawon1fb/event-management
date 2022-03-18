4aa23b67af7ecd61f84b7d90cf7bf874d085f83d
shawon1fb <shawon0187@gmail.com>
2022-03-18 22:44:15 +0600

    - create profile dto

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,11 @@
-export class CreateProfileDto {}
+export class CreateProfileDto {
+  bio: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+  job_title: string;
+  school: string;
+  gender: string;
+  city: string;
+  passion: string[];
+}
