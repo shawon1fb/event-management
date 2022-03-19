a399894467358bc7b0bff6a4394f12166c14824e
shawon1fb <shawon0187@gmail.com>
2022-03-20 00:32:52 +0600

    - multi part request 1

--- src/profile/dto/update-profile.dto.ts
+++ src/profile/dto/update-profile.dto.ts
@@ -1,14 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProfileDto } from './create-profile.dto';
 
-export class UpdateProfileDto extends PartialType(CreateProfileDto) {
-  // bio: string;
-  // firstname: string;
-  // lastname: string;
-  // username: string;
-  // job_title: string;
-  // school: string;
-  // gender: string;
-  // city: string;
-  // passion: string[];
-}
+export class UpdateProfileDto extends PartialType(CreateProfileDto) {}
