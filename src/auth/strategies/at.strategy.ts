763d9d01f84572d956fe42d714f143f03d95ebc8
shawon1fb <shawon0187@gmail.com>
2022-03-26 17:58:11 +0600

    update jwt strategy

--- src/auth/strategies/at.strategy.ts
+++ src/auth/strategies/at.strategy.ts
@@ -5,12 +5,14 @@ import { ConfigService } from '@nestjs/config';
 import { JwtPayload } from '../types';
 import { PrismaService } from '../../prisma/prisma.service';
 import { User } from '@prisma/client';
+import { RedisService } from '../../redis/redis.service';
 
 @Injectable()
 export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(
     private configService: ConfigService,
     private prisma: PrismaService,
+    private cacheService: RedisService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -21,12 +23,25 @@ export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
   async validate(payload: JwtPayload): Promise<User> {
     const { email } = payload;
-    const user: User = await this.prisma.user.findUnique({
-      where: { email },
-      include: {
-        profile: true,
-      },
-    });
+
+    // const user: User = await this.prisma.user.findUnique({
+    //   where: { email },
+    //   include: {
+    //     profile: true,
+    //   },
+    // });
+
+    const user: User = await this.cacheService.remember(
+      `auth_user_${email}`,
+      3600,
+      () =>
+        this.prisma.user.findUnique({
+          where: { email },
+          include: {
+            profile: true,
+          },
+        }),
+    );
 
     if (!user) {
       throw new UnauthorizedException('unauthorized');
